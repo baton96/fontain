0bd3b266033f535dfa5f29d0e891ca4318c2961d
baton96 <podolce@op.pl>
2021-09-24 00:42:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# ⛲ fountain ⛲
+
+fountain is a Python tool for font recognition on images
+
+# Installation
+```pip install fountain```
+
+# Usage
+
+## API
+```python
+from fountain import *
+```
+
+## CLI
+```
+fountain [-h] path
+
+Recognize font on image
+
+positional arguments:
+  path                  input file path
+
+optional arguments:
+  -h, --help            show this help message and exit
+```
+
+# Example
+
+# License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='fountain',
+    packages=['fountain'],
+    version='0.1.1',
+    license='MIT',
+    description='Python tool for font recognition on images',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Bartosz Paulewicz',
+    author_email='podolce0@gmail.com',
+    url='https://gitlab.com/baton96/fountain',
+    download_url='https://gitlab.com/baton96/fountain/-/archive/0.1/fountain-0.1.tar.gz',
+    keywords=['font', 'ocr'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
